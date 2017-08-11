bf7be830cf694088b0aa6f43cf08b69e43c06c36
Pathak, Sumit <sumit.pathak@rakuten.com>
2017-08-11 13:05:54 +0530

    Coin change problem recursive sol added

---
+++ CoinChangeProblemDP.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class CoinChangeProblemDP{
+    int P = 4;
+    private static int[] denominations = {2, 3, 5};
+    public static void main(String[] args){
+        System.out.println("Minimum no of coins required :"+calculateMinCoinsRec(denominations, denominations.length, 5));
+    }
+    private static int calculateMinCoinsRec(int[] denom, int m, int sum){
+        if(sum==0) return 0; //base case 
+        int res = Integer.MAX_VALUE;
+        for(int i=0;i<m;i++){
+            if(denom[i]<=sum){
+                int s_res = calculateMinCoinsRec(denom, m, sum-denom[i]);
+                if(s_res!=Integer.MAX_VALUE && s_res+1<res)
+                    res = s_res+1;
+            }
+        }
+        return res;
+    }
+
+}
